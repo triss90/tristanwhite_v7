ac0febfa0a1fd96ea84731c1bc91c783f87f3918
Tristan White <oliver.tristan@gmail.com>
2016-05-21 13:44:33 +0200

    Adding work sections

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,6 @@
-
-
+/**
+  Navigation
+**/
 function navigation() {
   var toggle = $('#menu-toggle'),
       menu = $('#menu');
@@ -15,8 +16,76 @@ function navigation() {
 
 }
 
+
+/**
+  Carousel
+**/
+function carousel(){
+
+  function getTallestUnitHeight(){
+    var heights = [];
+    for (var i = 0; i < unitCount; i++) {
+      var height = $carouselUnits.eq(i).height();
+      heights.push(height);
+    }
+    var tallest = Math.max.apply(Math, heights);
+    return tallest;
+  }
+
+  var $carouselWrap = $('.carousel-wrap'),
+      $carouselUnits = $carouselWrap.find('.carousel-unit'),
+      unitCount = $carouselUnits.length,
+      tallestUnitHeight = getTallestUnitHeight(),
+      $carouselNav = $('.carousel-nav');
+
+
+
+  function initCarousel(){
+    $carouselUnits.height(tallestUnitHeight).first().addClass('is-current');
+    $carouselWrap.height(tallestUnitHeight);
+  }
+  initCarousel();
+
+  function nextTestimonial(){
+    if ($carouselUnits.filter('.is-current').index() < unitCount-1) {
+        $carouselUnits.filter('.is-current').next().addClass('is-current').siblings().removeClass('is-current');
+    }
+    else {
+      $carouselUnits.first().addClass('is-current').siblings().removeClass('is-current');
+    }
+  }
+
+  function prevTestimonial(){
+    if ($carouselUnits.filter('.is-current').index() === 0) {
+      $carouselUnits.last().addClass('is-current').siblings().removeClass('is-current');
+    }
+    else {
+      $carouselUnits.filter('.is-current').prev().addClass('is-current').siblings().removeClass('is-current');
+    }
+  }
+
+  var carouselInterval = setInterval(function(){
+    nextTestimonial();
+  }, 4500);
+
+  $carouselNav.children('svg').on('click', function(){
+    clearInterval(carouselInterval);
+    if ($(this).index() === $carouselNav.length) {
+      nextTestimonial();
+    } else {
+      prevTestimonial();
+    }
+  });
+
+}
+
+
+/**
+  Run functions
+**/
 $(function() {
 
   navigation();
+  carousel();
 
 });
